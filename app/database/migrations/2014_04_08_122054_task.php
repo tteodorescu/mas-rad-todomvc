9b2c2dfbb8ae8fa87ceabb0d6da85fdaf85dacd4
traianteodorescu <traian.teodorescu@bluewin.ch>
2014-04-08 17:52:04 +0000

    deplace 1

--- app/database/migrations/2014_04_08_122054_task.php
+++ app/database/migrations/2014_04_08_122054_task.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Tasks extends Migration {
+class Tasks_1 extends Migration {
 
 	/**
 	 * Run the migrations.
