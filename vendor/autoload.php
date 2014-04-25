4111c24c7f1d85688d3de64141ff597d29409a66
traianteodorescu <traian.teodorescu@bluewin.ch>
2014-04-25 18:47:22 +0000

    homework 5

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb5377e7531b21880e318fb989c34bed3::getLoader();
+return ComposerAutoloaderInitd5d57245381973fd6f8efc4d0c01835d::getLoader();
